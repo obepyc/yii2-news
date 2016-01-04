c21f010935eb73c60aabbc288970c98ddcb79eda
OBEPYC <obepyc@mail.ru>
2016-01-04 19:38:19 +0200

    DefualtRoute post

--- backend/config/main.php
+++ backend/config/main.php
@@ -12,6 +12,7 @@ return [
     'controllerNamespace' => 'backend\controllers',
     'bootstrap' => ['log'],
     'modules' => [],
+    'defaultRoute' => 'post/index',
     'components' => [
         'request' => [
             'baseUrl' => '/adminpanel',
